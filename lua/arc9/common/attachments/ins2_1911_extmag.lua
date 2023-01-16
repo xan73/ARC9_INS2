748b12160606e682266ca1ad564bb0e40f566b4c
xan73 <96928340+xan73@users.noreply.github.com>
2023-01-16 15:29:46 -0500

    Update ins2_1911_extmag.lua

--- lua/arc9/common/attachments/ins2_1911_extmag.lua
+++ lua/arc9/common/attachments/ins2_1911_extmag.lua
@@ -1,9 +1,12 @@
 ATT.PrintName = "Extended Magazine"
 ATT.CompactName = "15RND"
 ATT.Icon = Material("entities/", "mips smooth")
+ATT.Description = [["Bridges the gap between the higher capacity pistols. Has minor downsides."]]
 
 ATT.SortOrder = 0
 ATT.MenuCategory = "ARC9 - INS2 Attachments"
 ATT.ClipSize = 15
+
+ATT.ReloadTimeMult = 0.5
 
 ATT.Category = {"ins2_1911_extmag"}
